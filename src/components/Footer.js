65186b966a2cdf7a21ad44d50db104b43d917600
Debasish Mahapatra <agcsdebasish@gmail.com>
2023-06-20 02:13:09 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import styles from "styles/Footer.module.scss";
+import GetIcon from "components/GetIcon";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Debasish Mahapatra
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
